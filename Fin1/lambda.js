a0a9f923a485d6ee0226ed84e4d3bbaac334a27f
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-07-26 13:12:55 +0530

    test

--- Fin1/lambda.js
+++ Fin1/lambda.js
@@ -8,36 +8,11 @@ exports.handler = function (event, context, callback) {
         query: {},
         headers: { "Accept": "" }
     }).then((response) => {
+        callback(null, { "message": response });
         // your code goes here
         console.log(response);
     }).catch((err) => {
         // error handling goes here
     });
-
-    Swagger.http({
-        url: `http://bx8ocdir2g.execute-api.eu-west-1.amazonaws.com/prod/fyn`,
-        method: 'post',
-        query: {},
-        headers: { "Accept": "", "Content-Type": "applications/json" },
-        body: { "Test": "Value" }
-    }).then((response) => {
-        console.log(response);
-        // your code goes here
-    }).catch((err) => {
-        // error handling goes here
-    });
-    Swagger.http({
-        url: `http://bx8ocdir2g.execute-api.eu-west-1.amazonaws.com/prod/fyn`,
-        method: 'post',
-        query: {},
-        headers: { "Accept": "", "Content-Type": "applications/json" },
-        body: b
-    }).then((response) => {
-        // your code goes here
-    }).catch((err) => {
-        // error handling goes here
-    });
-
-
     callback(null, { "message": "Successfully executed" });
 }
