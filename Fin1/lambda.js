0328409cc371a9bfe2f95664671f570a41fee32c
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-07-26 13:32:16 +0530

    test

--- Fin1/lambda.js
+++ Fin1/lambda.js
@@ -2,17 +2,4 @@ let Swagger = require('swagger-client');
 
 exports.handler = function (event, context, callback) {
 
-    Swagger.http({
-        url: `http://bx8ocdir2g.execute-api.eu-west-1.amazonaws.com/prod/fyn`,
-        method: 'get',
-        query: {},
-        headers: { "Accept": "" }
-    }).then((response) => {
-        callback(null, { "message": response });
-        // your code goes here
-        console.log(response);
-    }).catch((err) => {
-        // error handling goes here
-    });
-    callback(null, { "message": "Successfully executed" });
 }
